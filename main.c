cc375c34d5eb4dda20db2a839b507cb243ff2bb7
KaylynnDavis <160663983+KaylynnDavis@users.noreply.github.com>
2024-03-25 14:28:49 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <kipr/wombat.h>
+#include <create_lib.h>
 #include <god_lib.h>
 #include <locolib.h>
-#include <constants.h>
-#include <run_functions.h>
+/*square_up(int ending,int speed);
+drive(int speed,int x(distance));
+turn(int speed,int deg, int rad);
+smart_servo(part,percentage,1.5,0); don't forget msleep!!!!!
+main functions used in this code for right now*/
 
 int main()
 {
-    god_start();
-    printf("end");
+    is_create();
+    
+    square_up(2,200);
+    drive(200,8);
+    turn(200,-90,360);
+    smart_servo(table,50,1.5,0);
+    msleep(2000);
+    
+    
+    printf("Hello World\n");
     return 0;
 }
